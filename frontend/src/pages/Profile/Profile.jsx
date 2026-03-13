f07a256a289a583214bc1c405afc50f66e6935ba
gfr-31 <158162524+gfr-31@users.noreply.github.com>
2026-03-13 15:57:06 +0700

    Update Profile.jsx

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useAuthStore from "../../store/authStore";
-import api from "../../services/api";
+import api from "../../api/axios";
 
 export default function Profile() {
   const { user, logout } = useAuthStore();
